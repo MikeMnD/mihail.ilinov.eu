52f04f34c479fca1c9b5798ddb826f644d16e021
Mihail Ilinov <mihail@ilinov.eu>
2017-01-09 12:25:41 +0200

    Added "php artisan make auth"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Mihail Ilinov</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -74,17 +74,42 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+
+                <div class="title m-b-md" title="Personal website of Mihail Ilinov -  Software Developer / Music Producer ">
+                    Mihail Ilinov
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <h2>Ways to contact me:</h2>
+                <h4>mail: <a href="mailto:mihail@ilinov.eu" title="If you're a bot, please don't spam me!">mihail@ilinov.eu</a></h4>
+                <h4>Phone: <a href="tel:+359887837569">+359 887 83 75 69</a></h4>
+
+                <h4>facebook: <a href="http://www.facebook.com/mihail.ilinov" target="_blank" title="Check my wall...another brick in the wall!">mihail.ilinov</a></h4>
+                <h4>google+: <a href="https://plus.google.com/116549256557288537186/about" target="_blank" title="Circle, circle dot dot...!">mihail.ilinov</a></h4>
+                <h4>linkedIn: <a href="https://www.linkedin.com/in/mihail-ilinov-4bb8b023" target="_blank" title="If you're a bot, please don't spam me!">Mihail Ilinov</a></h4>
+                <h4>twitter: <a href="http://twitter.com/#!/MikeMnd" target="_blank" title="Follow me on twitter!">MikeMnD</a></h4>
+                <h4>GitHUB: <a href="https://github.com/MikeMnD" target="_blank" title="Social coding is more than cool!">Me at GitHub</a></h4>
+
+                <h3>My own projects:</h3>
+                <div class="sitesHolder">
+                    <ul class="halfwidth">
+                        <li><a href="http://adelinapiano.com" target="_blank" title="Пиано уроци">adelinapiano.com</a></li>
+                        <li><a href="http://www.mig7studio.com" target="_blank" title="Звукозаписно студио и репетиционна">www.mig7studio.com</a></li>
+                        <li><a href="http://www.sdc-bg.com" target="_blank" title="Сайт за манга на български език">sdc-bg.com</a></li>
+                        <li><a href="http://www.html5piano.ilinov.eu" target="_blank" title="Пиано създадено с HTML5">html5piano.ilinov.eu</a></li>
+                    </ul>
                 </div>
+                <div style="clear: both;"></div>
+
+                <h3>My work at Calipers</h3>
+                <h4>Check latest projects from <a href="http://www.calipers.bg/bg/%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8/" target="_blank" title="Calipers - Interactive Agency">www.calipers.bg</a></h4>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Bio and skills</a>
+                    <a href="https://laracasts.com">Portfolio</a>
+                    <a href="https://laravel-news.com">Music</a>
+                    <a href="https://forge.laravel.com">Friends</a>
+                </div>
+
             </div>
         </div>
     </body>
